07806224fea4205a40794c590398b5ff6aceb786
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2020-07-11 11:56:54 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function menuopen(){
+    var menu = document.getElementById("open").style
+    if(menu.visibility == "hidden"){
+        menu.visibility="visible"
+    }else{
+        menu.visibility="hidden"
+    }
+}
